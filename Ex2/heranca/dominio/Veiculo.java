b6c423491168791a787c9998c7d6347647115e4e
WillPrata <wilson.prata@soulasalle.com.br>
2025-05-29 20:37:48 -0300

    Create Veiculo.java

---
+++ Ex2/heranca/dominio/Veiculo.java
@@ -0,0 +1,41 @@
+package Exercicios02.heranca2.dominio;
+
+
+public class Veiculo {
+    protected String marca;
+    protected String modelo;
+    protected int ano;
+
+
+    public Veiculo(String marca, String modelo, int ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+    public String getModelo() {
+        return modelo;
+    }
+    public int getAno() {
+        return ano;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+    public void exibirDados() {
+        System.out.println("Marca: " + this.marca);
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Ano: " + this.ano);
+    }
+}
+
+
